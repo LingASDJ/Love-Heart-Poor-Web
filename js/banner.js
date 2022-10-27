50af355e34e4e8ac4e0af7ae7631e000205c970c
LingASDJ <2735951230@qq.com>
2022-10-27 15:58:13 +0800

    ok

---
+++ js/banner.js
@@ -0,0 +1,35 @@
+// 定义一个数组，存放图片地址
+let arr = [
+    "background:url(../img/index/banner/banner2.png) center",
+    "background:url(../img/index/banner/banner3.png) center",
+    "background:url(../img/index/banner/banner4.png) center",
+]
+// 让arr数组里的元素执行      foreach循环：数组每个元素都执行一次回调函数。
+arr.forEach(function () {
+    // 选择.num，在后面添加标签内容   append方法在被选元素的结尾插入指定内容。
+    $(".num").append('<span></span>')
+})
+//     first() 方法返回被选元素的第一个元素   addClass() 方法向被选元素添加一个或多个类名。
+$(".num span").first().addClass("on")
+
+
+let i = 0;
+let timer = null
+
+function run() {
+
+    // setInterval() - 间隔指定的毫秒数不停地执行指定的代码。
+    // clearInterval() 方法用于停止 setInterval() 方法执行的函数代码。
+    timer = setInterval(function () {
+        i++;
+        if (i === arr.length) {
+            i = 0
+        }
+        $("#guang").attr("style", arr[i])
+        // 选择到目标位置，将样式添加到eq选择到的上面
+        // siblings() 方法返回被选元素的所有同级元素。  也就是说把.num下的所有span
+        $(".num>span").eq(i).addClass("on").siblings().removeClass("on")
+
+    }, 2000)
+}
+run()
